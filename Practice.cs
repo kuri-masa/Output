c1d3a6ede34c4db5bc8f9fc6bf2948bb1dc7c3a3
kuri_masa <bigmaron39.56.72@gmail.com>
2022-03-31 19:44:54 +0900

    ログに数字(second)を表示。

---
+++ Practice.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    float second = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DispleyNumber();
+    }
+
+    void DispleyNumber()
+    {
+        Debug.Log(second);
+    }
+
+    void Update()
+    {
+        
+    }
+}
