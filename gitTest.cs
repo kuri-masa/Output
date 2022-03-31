082e546faa3de449f02c75008a280d5011d8670b
kuri_masa <bigmaron39.56.72@gmail.com>
2022-03-31 18:28:13 +0900

    test comit

---
+++ gitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gitTest : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log(123);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
