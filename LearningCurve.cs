c47e580d2fdea15ecf909df92f34cc910b0a4059
kuri_masa <bigmaron39.56.72@gmail.com>
2022-04-12 23:33:00 +0900

    ComputeAge

--- LearningCurve.cs
+++ LearningCurve.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    private int currentAge = 30;
+    public int currentAge = 30;
     public int addedAge = 1;
-
+    
     public float pi = 3.14f;
     public string firstName = "Harrison";
     public bool isAuthor = true;
 
+
+    void ComputeAge()
+    {
+        Debug.Log(5 + 4 - 3 / 2 * 1);
+        Debug.Log(5 + (4 - 3) / 2 * 1);
+    }
+
     void Start()
     {
-        ComputeAge();
-
         Debug.Log($"文字列には{firstName}のような変数を挿入できる！");
-        
+
+        //Debug.Log(firstName * pi);
     }
 
     // Update is called once per frame
@@ -25,8 +31,5 @@ public class LearningCurve : MonoBehaviour
         
     }
 
-    void ComputeAge()
-    {
-        Debug.Log(currentAge + addedAge);
-    }
+    
 }
